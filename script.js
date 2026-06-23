7ee2a0edb3107d1145fe5e19ad501f81ebff04e2
riadh100 <riadh-mekni1@outlook.de>
2026-06-23 22:38:03 +0200

    Refactor openPage to openTool and clean up code
    Renamed function openPage to openTool and removed unused functions.

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-function openPage(page){
+function openTool(page){
 
 window.location.href = page;
-
-}
-
-function frageMich(){
-
-alert("Hallo Riadh. Wie kann ich dir helfen?");
-
-}
-
-function comingSoon(name){
-
-alert(name + " kommt bald.");
-
-}
-
-function openWebsite(link){
-
-window.open(link,"_blank");
 
 }
