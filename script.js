efa3fe59a963c9438464d55ca746302ee0ae06e1
riadh100 <riadh-mekni1@outlook.de>
2026-06-23 20:14:38 +0200

    Add navigation for multiple tools in openTool function

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function openTool(name){
 
-alert(name + " wird geöffnet");
+if(name==="Trading Center"){
+window.location.href="trading.html";
+}
+
+else if(name==="Tages-Setups"){
+window.location.href="setup.html";
+}
+
+else if(name==="Telegram Center"){
+window.location.href="telegram.html";
+}
+
+else if(name==="Video Studio"){
+window.location.href="video.html";
+}
+
+else if(name==="Game Studio"){
+window.location.href="game.html";
+}
+
+else if(name==="KI Werkzeuge"){
+window.location.href="tools.html";
+}
 
 }
