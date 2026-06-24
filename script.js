3ef952eab16f447398c5d406bd44d48ca9eb9af3
riadh100 <riadh-mekni1@outlook.de>
2026-06-24 18:14:45 +0200

    Add task creation functionality in script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function openTool(page){
+function addTask() {
 
-window.location.href = page;
+const input = document.getElementById("taskInput");
+
+const task = input.value.trim();
+
+if(task === "") return;
+
+const list = document.getElementById("taskList");
+
+const li = document.createElement("li");
+
+li.innerHTML = `
+<input type="checkbox">
+<span>${task}</span>
+`;
+
+list.appendChild(li);
+
+input.value = "";
 
 }
