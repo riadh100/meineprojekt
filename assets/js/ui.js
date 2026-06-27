65aa2a44a22a45a10cb0484bce71922233a6257d
riadh100 <riadh-mekni1@outlook.de>
2026-06-27 14:06:14 +0200

    Add UI manager initialization in ui.js

---
+++ assets/js/ui.js
@@ -0,0 +1,19 @@
+/**
+ * =====================================================
+ * AI Empire Pro V8
+ * UI Manager
+ * Datei: assets/js/ui.js
+ * =====================================================
+ */
+
+const UI = {
+
+    init() {
+
+        this.updateClock();
+
+        this.startClock();
+
+        this.initTooltips();
+
+        this
